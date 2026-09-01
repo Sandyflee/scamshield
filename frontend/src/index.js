4e438c3c0c330db7ffa4a11125ed2363e2164d3b
Aganyi Sandra <129175392+Sandyflee@users.noreply.github.com>
2026-09-01 09:44:57 +0100

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// Google Analytics
+const GA_MEASUREMENT_ID = 'G-J60109867M';
+
+const script = document.createElement('script');
+script.async = true;
+script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`;
+document.head.appendChild(script);
+
+window.dataLayer = window.dataLayer || [];
+
+function gtag() {
+  window.dataLayer.push(arguments);
+}
+
+gtag('js', new Date());
+gtag('config', GA_MEASUREMENT_ID);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <App />
